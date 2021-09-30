d9b2f0b328da3249378597db220f3bcba4367aa7
bradyrussell <31458670+bradyrussell@users.noreply.github.com>
2021-09-29 19:48:48 -0500

    Add project files.

---
+++ TSqlToolsLibDotNET/TSqlDescriptionFromCommentsListener.cs
@@ -0,0 +1,113 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace TSqlToolsLib
+{
+    class TSqlDescriptionFromCommentsListener : TSqlParserBaseListener
+    {
+        private static string descriptionCommentBeginSymbol = "$";
+        private static string descriptionCommentEndSymbol = "$";
+
+        private CommonTokenStream tokens;
+        private TokenStreamRewriter rewriter;
+        private List<string> columnDescriptions = new List<string>();
+
+        private string currentTable = null;
+        private string currentSchema = null;
+
+        public TSqlDescriptionFromCommentsListener(CommonTokenStream tokens)
+        {
+            this.tokens = tokens;
+            this.rewriter = new TokenStreamRewriter(tokens);
+        }
+
+        public override void EnterCreate_table(TSqlParser.Create_tableContext ctx)
+        {
+            if (ctx.table_name() == null) return;
+            if (ctx.table_name().schema != null)
+            {
+                currentSchema = RemoveBrackets(ctx.table_name().schema.GetText());
+            }
+            else
+            {
+                currentSchema = null;
+            }
+
+            if (ctx.table_name().table != null)
+            {
+                currentTable = RemoveBrackets(ctx.table_name().table.GetText());
+            }
+            else
+            {
+                currentTable = null;
+            }
+            Debug.WriteLine("Current Schema: "+ currentSchema);
+            Debug.WriteLine("Current Table: "+ currentTable);
+        }
+
+
+        public override void EnterColumn_definition(TSqlParser.Column_definitionContext ctx)
+        {
+            string columnName = ctx.id_()[0].GetText().Replace("[", "").Replace("]", "");
+            Debug.WriteLine(columnName);
+
+            int offset = 0;
+            if (tokens.Get(ctx.Stop.TokenIndex + 1).Type == TSqlParser.COMMA)
+            {
+                offset = 1;
+            }
+
+            IList<IToken> comments = tokens.GetHiddenTokensToRight(ctx.Stop.TokenIndex + offset);
+
+            if (comments == null) return;
+
+            if (comments.Count > 1)
+            {
+                throw new Exception("Multiple comments not allowed. Line: " + comments[0].Line);
+            }
+
+            if (comments.Count == 1)
+            {
+                string commentText = comments[0].Text;
+                Debug.WriteLine("Found comment: "+ commentText);
+
+                if (commentText.Contains(descriptionCommentBeginSymbol))
+                {
+                    Debug.WriteLine("Comment contains description!");
+                    int index = commentText.IndexOf(descriptionCommentBeginSymbol);
+                    string descriptionFromComment = commentText.Substring(index + descriptionCommentBeginSymbol.Length);
+                    if (descriptionFromComment.Contains(descriptionCommentEndSymbol))
+                    {
+                        int index2 = descriptionFromComment.IndexOf(descriptionCommentEndSymbol);
+                        descriptionFromComment = descriptionFromComment.Substring(0, index2);
+                    }
+                    Debug.WriteLine("Description for column " + columnName + ": " + descriptionFromComment);
+
+                    columnDescriptions.Add(MakeDescription(currentSchema, currentTable, columnName, descriptionFromComment.Replace("'", "''")));
+                }
+                else
+                {
+                    Debug.WriteLine("Comment is regular comment!");
+                }
+            }
+        }
+
+        public List<string> GetColumnDescriptions()
+        {
+            return columnDescriptions;
+        }
+
+        private string RemoveBrackets(string input)
+        {
+            return input.Replace("[", "").Replace("]", "");
+        }
+
+        public string MakeDescription(string schema, string table, string column, string description)
+        {
+            return $"EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'{description}' , @level0type=N'SCHEMA',@level0name=N'{schema ?? ""}', @level1type=N'TABLE',@level1name=N'{table ?? ""}', @level2type=N'COLUMN',@level2name=N'{column}'\nGO";
+        }
+    }
+}
---
+++ TSqlToolsLibDotNET/TSqlTools.cs
@@ -0,0 +1,22 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System;
+
+namespace TSqlToolsLib
+{
+    public class TSqlTools
+    {
+        public static string GenerateDescriptions(string input)
+        {
+            ICharStream stream = CharStreams.fromString(input);
+            ITokenSource lexer = new TSqlLexer(new CaseChangingCharStream(stream, true));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            TSqlParser parser = new TSqlParser(tokens);
+            parser.BuildParseTree = true;
+            var listener = new TSqlDescriptionFromCommentsListener(tokens);
+            ParseTreeWalker.Default.Walk(listener, parser.tsql_file());
+            var x = listener.GetColumnDescriptions().ToArray();
+            return string.Join("\n", x);
+        }
+    }
+}
